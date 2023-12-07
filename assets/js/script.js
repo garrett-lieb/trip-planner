a621e7b8cbc11c79745e789cef01078b376815e3
garrett lieb <liebg08@gmail.com>
2023-12-06 21:04:07 -0800

    commented out java notes waiting for api key to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
-// amadeus for developers key: 
+// amadeus for developers: 
+// FGQpACb38YCjVYJujicKvlCD3Xu9j6GQ
+// HscHt2GDGE1YEY5c
 
 
-curl "https://test.api.amadeus.com/v1/security/oauth2/token" \
-     -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "grant_type=client_credentials&client_id={FGQpACb38YCjVYJujicKvlCD3Xu9j6GQ}&client_secret={HscHt2GDGE1YEY5c}"
+// curl "https://test.api.amadeus.com/v1/security/oauth2/token" \
+//      -H "Content-Type: application/x-www-form-urlencoded" \
+//      -d "grant_type=client_credentials&client_id={FGQpACb38YCjVYJujicKvlCD3Xu9j6GQ}&client_secret={HscHt2GDGE1YEY5c}"
