ac609eb60310ed0912a7b5d481c1b27127f93002
garrett lieb <liebg08@gmail.com>
2023-12-06 21:17:08 -0800

    trying to add API keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,13 @@
 // FGQpACb38YCjVYJujicKvlCD3Xu9j6GQ
 // HscHt2GDGE1YEY5c
 
+// tomtom api
+// J6oSNBsm26G60qblIMVB5AqP7oduBIc3
+
 
 // curl "https://test.api.amadeus.com/v1/security/oauth2/token" \
 //      -H "Content-Type: application/x-www-form-urlencoded" \
 //      -d "grant_type=client_credentials&client_id={FGQpACb38YCjVYJujicKvlCD3Xu9j6GQ}&client_secret={HscHt2GDGE1YEY5c}"
+
+// tom tom fuel prices
+// curl 'https://api.tomtom.com/search/2/fuelPrice.json?key={J6oSNBsm26G60qblIMVB5AqP7oduBIc3}&fuelPrice=1:2622f89a-6300-11ec-8d12-a0423f39b5a2'
