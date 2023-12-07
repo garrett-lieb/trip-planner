0bc1031fa36fdb43f737cdbca2c1e4832c00ee9e
garrett lieb <liebg08@gmail.com>
2023-12-06 20:55:46 -0800

    pasted api keys into curl request and saved in js file; still waiting for api key to activate

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// amadeus for developers key: 
+
+
+curl "https://test.api.amadeus.com/v1/security/oauth2/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id={FGQpACb38YCjVYJujicKvlCD3Xu9j6GQ}&client_secret={HscHt2GDGE1YEY5c}"
